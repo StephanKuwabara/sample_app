d2724ce7846126a00ff92648382753b146980bca
EC2 Default User <ec2-user@ip-172-31-41-61.us-east-2.compute.internal>
2018-01-26 19:21:55 +0000

    fix tabbing error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   end
   
   def create
-        @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
